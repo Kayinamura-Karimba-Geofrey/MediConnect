9106cf911b8db9a679b345d4027b05a496f8113e
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-11-21 16:59:01 +0200

    constant

--- health_platform/src/main/java/com/example/health_platform/util/Constants.java
+++ health_platform/src/main/java/com/example/health_platform/util/Constants.java
@@ -1,5 +1,14 @@
 package com.example.health_platform.util;
 
 public class Constants {
-    
+
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_DOCTOR = "DOCTOR";
+    public static final String ROLE_NURSE = "NURSE";
+    public static final String ROLE_PATIENT = "PATIENT";
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    public static final long JWT_EXPIRATION = 86400000; 
 }
