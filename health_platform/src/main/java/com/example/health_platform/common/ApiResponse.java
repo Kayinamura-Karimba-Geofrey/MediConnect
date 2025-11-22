355a1813a9dbc643bf2f5ff68a0cf39accfd068c
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-11-23 01:28:37 +0200

    comments

--- health_platform/src/main/java/com/example/health_platform/common/ApiResponse.java
+++ health_platform/src/main/java/com/example/health_platform/common/ApiResponse.java
@@ -14,10 +14,5 @@ public class ApiResponse<T> {
         this.success = success; this.data = data; this.message = message;
     }
     
-    // public boolean isSuccess() { return success; }
-    // public void setSuccess(boolean success) { this.success = success; }
-    // public T getData() { return data; }
-    // public void setData(T data) { this.data = data; }
-    // public String getMessage() { return message; }
-    // public void setMessage(String message) { this.message = message; }
+    
 }
