55bd6c1e45f3d46bb8a28aa736900491c5b275a0
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-11-23 19:52:54 +0200

    swagger

--- health_platform/src/main/java/com/example/health_platform/config/SwaggerConfig.java
+++ health_platform/src/main/java/com/example/health_platform/config/SwaggerConfig.java
@@ -1,12 +1,14 @@
 package com.example.health_platform.config;
 
-
-
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,7 +24,14 @@ public class SwaggerConfig {
                                 .name("HealthSphere Support")
                                 .email("support@healthsphere.com")
                         )
-                );
+                        .license(new License()
+                                .name("Apache License 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")
+                        )
+                )
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Server"),
+                        new Server().url("https://healthsphere.com/api").description("Production Server")
+                ));
     }
 }
-
